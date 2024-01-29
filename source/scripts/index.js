54d612cac2686ecef6a3ea0eaf47bb93724bf482
bobchinskiy <bobchisnkiy@gmail.com>
2024-01-29 21:57:04 +0500

    Сделал декор, разюираюсь с сладйером и бургером

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
-/* в этот файл добавляет скрипты*/
+const slider = document.getElementById('slider');
+const afterWrapper = document.querySelector('.example__slider-after-wrapper');
+
+slider.addEventListener('input', () => {
+  const value = slider.value;
+  afterWrapper.style.clipPath = `inset(0 ${100 - value}% 0 0)`;
+});
