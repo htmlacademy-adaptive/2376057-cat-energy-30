895259499e9cca516044a8266498b4d79c234fb9
bobchinskiy <bobchisnkiy@gmail.com>
2024-01-31 00:43:22 +0500

    Сделал adress example и burger. Остался пиксель перфект

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,7 +1,14 @@
-const slider = document.getElementById('slider');
-const afterWrapper = document.querySelector('.example__slider-after-wrapper');
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.burger-toggle');
 
-slider.addEventListener('input', () => {
-  const value = slider.value;
-  afterWrapper.style.clipPath = `inset(0 ${100 - value}% 0 0)`;
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
 });
